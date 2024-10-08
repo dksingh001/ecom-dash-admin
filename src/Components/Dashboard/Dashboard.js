0a4f36ec3f92f649a92faa4d71dbdd2d2ce29c24
Dinesh Singh <dkadesh1999@gmail.com>
2024-10-08 20:35:01 +0530

    first commit

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+
+const Dashboard = () => {
+    return (
+        <>
+          <div className='dsP'>
+           <div className='dsC'>
+            <div className='dsbright'>
+             <div className='dsbrightN'>
+               Hi Dinesh 
+             </div>
+             <div className='dsbrightDP'>
+              <div className='dsbrightAdd'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightAddP'>Add Product</div>
+              </div>
+              <div className='dsbrightUp'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightUP'>Update Product</div>
+              </div>
+              <div className='dsbrightDe'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightDP'>Delete Product</div>
+              </div>
+              <div className='dsbrightAllC'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightAllc'>All Customers</div>
+              </div>
+              <div className='dsbrightLiP'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightLip'>Live Order</div>
+              </div>
+              <div className='dsbrightToO'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightToo'>Total Order</div>
+              </div>
+              <div className='dsbrightCmO'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightCmO'>Complete Order</div>
+              </div>
+              <div className='dsbrightCanO'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightCano'>Cancel Order</div>
+              </div>
+              <div className='dsbrightCanoID'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightCanId'>Create Another ID</div>
+              </div>
+              <div className='dsbrightlogout'>
+                <div className='dsbrightI'>Icon</div>
+                <div className='dsbrightlogout'>Logout</div>
+              </div>
+             </div>
+            </div>
+            <div className='dsbleft'>
+             
+            </div>
+           </div>
+          </div>
+        </>
+    );
+}
+
+export default Dashboard;
